63602babf2e9e9db8508bfc629effcb3e39b3f5a
sdev0011 <sdev30mishra@gmail.com>
2020-04-13 16:23:09 +0530

    updated all

---
+++ tech_blog/__init__.py
@@ -0,0 +1,58 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+
+#############################################################################
+############ CONFIGURATIONS  ###############
+###########################################################################
+
+
+app.config['SECRET_KEY'] = 'mysecret'
+
+#################################
+### DATABASE SETUPS ############
+###############################
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+
+###########################
+#### LOGIN CONFIGS #######
+#########################
+
+login_manager = LoginManager()
+
+# We can now pass in our app to the login manager
+login_manager.init_app(app)
+
+# Tell users what view to go to when they need to login.
+login_manager.login_view = "users.login"
+
+
+###########################
+#### BLUEPRINT CONFIGS #######
+#########################
+
+# Import these at the top if you want
+# We've imported them here for easy reference
+from tech_blog.core.views import core
+from tech_blog.users.views import users
+from tech_blog.blog_posts.views import blog_posts
+from tech_blog.error_pages.handlers import error_pages
+
+# Register the apps
+app.register_blueprint(users)
+app.register_blueprint(blog_posts)
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
